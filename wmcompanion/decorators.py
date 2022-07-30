8ba6280424f373b091e731ac325b99ef14cd1450
Daniel Pereira <daniel@garajau.com.br>
2022-07-30 02:02:21 -0300

    fix: major issue at decorators
    Previously, the implementation was limiting the usage of each decorator
    by one, because it was assuming that a single instance of a decorator
    would be equal to a single usage of it in a function. However that
    assumption is *WRONG* because a decorator gets instantiated once and
    then applied multiple times.
    
    This fixes it by moving the decorator metadata to the function object
    instead of keeping it in the single tenant instance of the decorator
    object.

--- wmcompanion/decorators.py
+++ wmcompanion/decorators.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import functools, types
+from collections import namedtuple
 from .object_container import ObjectContainer
 from .event_listening import EventWatcher
 
@@ -17,34 +18,34 @@ class SoftDecorator:
     easily, it is not always wanted in case we want to stack up multiple decorators that may be
     dependent and require some coordination such as order of execution. As a parallel, in languages
     such as Java for instance, there's no such thing as "behavior-changing metadata", and instead we
-    have annotations which are used in a similar way to decorators, but they don't automatically
-    wrap and modify functions as a decorator in Python would, instead that is up to the application
-    to read that metadata and act upon it.
+    have annotations which syntactically similar to decorators, but they don't automatically wrap
+    and modify functions as a decorator in Python would, instead that is up to the application to
+    read that metadata and act upon it.
 
     This class is the simplest implementation I could think of to create the idea of `annotation`
     you have on Java, using Python decorators. The naming embodies the idea of being `soft` as a way
-    to state that this does not automatically change the function, instead it only creates a new
+    to say that this does not automatically change the function, instead it only creates a new
     function with the expected behavior applied when you call `with_decorators()` on the function.
     """
     FUNCTION_ATTR_KEY_NAME = '_annotation_decorators'
+
+    DecoratorEnvelope = namedtuple('DecoratorEnvelope', ['object', 'args', 'kwargs'])
 
     def __call__(self, *args, **kwargs):
         """
         Makes this object callable. It is supposed to be called when applied to a function as a
         decorator.
-        When called, it saves the arguments passed to the decorator as internal properties (args and
-        kwargs) and adds this decorator to the function metadata.
+        When called, it saves this decorator object and the arguments (args and kwargs) passed to
+        the decorator as internal properties to the function metadata.
         """
-        def decorator(function):
-            self.args = args
-            self.kwargs = kwargs
-            self.add_self_reference(function)
-            self.after_declared()
+        def decorator(function: callable):
+            self.add_self_reference(function, args, kwargs)
+            self.after_declared(function, args, kwargs)
             return function
 
         return decorator
 
-    def after_declared(self):
+    def after_declared(self, function: callable, args: list, kwargs: dict):
         """
         Hook called after a decorator has been added to the function. It is convenient so that we
         can i.e. add the function to a callback list, but it can't change the function behavior, for
@@ -54,11 +55,11 @@ class SoftDecorator:
         """
         pass
 
-    def add_self_reference(self, function):
+    def add_self_reference(self, function: callable, args: list, kwargs: dict):
         """
-        Links the function to this decorator and vice-versa. Adds a new method (with_decorators) to
-        the function object so that when called, it will return a new function with all decorators
-        applied.
+        Links the function to this decorator and its arguments. Adds a new method (with_decorators)
+        to the function object so that when called, it will return a new function with all
+        decorators applied.
         """
         if not hasattr(function, self.FUNCTION_ATTR_KEY_NAME):
             setattr(function, self.FUNCTION_ATTR_KEY_NAME, [])
@@ -70,21 +71,30 @@ class SoftDecorator:
                 declared.
                 """
                 function = self
-                for decorator in reversed(getattr(self, function_attr_key_name)):
-                    function = decorator.apply(function)
+                for decorator_envelope in reversed(getattr(self, function_attr_key_name)):
+                    function = decorator_envelope.object.apply(
+                        function,
+                        decorator_envelope.args,
+                        decorator_envelope.kwargs
+                    )
                 return function
 
+            # All function stages while applying the decorators will have a reference to the
+            # original one, and a new method to apply all decorators
+            function.original_function = function
             function.with_decorators = types.MethodType(with_decorators, function)
 
-
         # Add a doubly linked reference between the function and the decorator object
-        self.original_function = function
-        getattr(function, self.FUNCTION_ATTR_KEY_NAME).append(self)
+        decorator_envelope = self.DecoratorEnvelope(self, args, kwargs)
+        getattr(function, self.FUNCTION_ATTR_KEY_NAME).append(decorator_envelope)
 
-    def apply(self, function: callable):
+    def apply(self, function: callable, args: list, kwargs: dict):
         """
         This is the decorator logic that should be applied to the function. It is only applied after
         calling `with_decorators()` method on the function object.
+
+        args - Is the list of arguments passed to the decorator (@dec(arg1, arg2))
+        kwargs - Is a dict with the keyword arguments passed to the decorator (@dec(arg="val"))
         """
         return function
 
@@ -95,9 +105,9 @@ class UseDecorator(SoftDecorator):
     def __init__(self, object_container: ObjectContainer):
         self.object_container = object_container
 
-    def apply(self, function: callable):
+    def apply(self, function: callable, args: list, kwargs: dict):
         curried_function = function
-        for dep in self.args:
+        for dep in args:
             obj = self.object_container.get(dep)
             # Set the object to the leftmost parameter of the function
             curried_function = functools.partial(curried_function, obj)
@@ -123,12 +133,12 @@ class OnDecorator(SoftDecorator):
     def __init__(self, event_watcher: EventWatcher):
         self.event_watcher = event_watcher
 
-    def after_declared(self):
-        event_klass = self.args[0]
-        attributes = self.kwargs
-        self.event_watcher.add_callback([event_klass, attributes], self.original_function)
+    def after_declared(self, function: callable, args: list, kwargs: dict):
+        event_klass = args[0]
+        attributes = kwargs
+        self.event_watcher.add_callback([event_klass, attributes], function)
 
-    def apply(self, function: callable):
+    def apply(self, function: callable, _args: list, _kwargs: dict):
         # Set the event object to the leftmost parameter of the function. The `event_object` is an
         # attribute set to the function by the EventListener when it triggers the function. After
         # used, let's clean it up so we don't end up with a property we don't want to keep around.
@@ -136,4 +146,4 @@ class OnDecorator(SoftDecorator):
         curried_function = functools.partial(function, event_object)
         if event_object: del function.event_object
         # Now wrap it and make it look like the original function
-        return functools.update_wrapper(curried_function, self.original_function)
+        return functools.update_wrapper(curried_function, function.original_function)
